fcbf4b3485734a23ced75ba6e84fa38f342fc2c2
Alex Milanov <alex@i4web.biz>
2012-01-19 16:23:04 +0200

    Added Ivelin's code for lecture 5

---
+++ examples/lecture_5/Zodiac_Ivelin/Classes/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  RootViewController.h
+//  Zodiac
+//
+//  Created by Ivelin Georgiev on 1/18/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@interface RootViewController : UITableViewController {
+}
+
+- (void)btnClicked:(UIButton *)sender;
+
+@end
